bc0058ba0621a6a54d2b243f7633ef45a33f93e9
harshmalu9 <hmalu975@gmail.com>
2025-09-19 19:14:53 +0530

    ui completely ready, bugs in the currency selector and currency value parsing

---
+++ CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+
+        fetch(url).then((response) => response.json())
+        .then((response) => setData(response[currency]))
+    },  [currency]);
+    return data;
+}
+
+export default useCurrencyInfo;
