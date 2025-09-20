0900647a739d2cd133b32142bde754ea70b59d61
harshmalu9 <hmalu975@gmail.com>
2025-09-20 19:01:09 +0530

    added new routes and components

--- ReactRouterBasics/src/components/Header/Header.jsx
+++ ReactRouterBasics/src/components/Header/Header.jsx
@@ -35,21 +35,39 @@ export default function Header() {
               <li>
                 <NavLink
                 to="/"
-                  className={(isActive) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive? 'text-orange-700' : 'text-gray-700'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                  }
+                className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 
+                    ${isActive ? 'text-orange-700' : 'text-gray-700'} 
+                    border-b border-gray-100 hover:bg-gray-50 
+                    lg:hover:bg-transparent lg:border-0 
+                    hover:text-orange-700 lg:p-0`
+                }
                 >
-                  Home
+                Home
                 </NavLink>
               </li>
               <li>
                 <NavLink
                 to="/about"
-                  className={(isActive) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive? 'text-orange-700' : 'text-gray-700'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  className={({isActive}) =>
+                    `block py-2 pr-4 pl-3 duration-200 
+                    ${isActive? 'text-orange-700' : 'text-gray-700'} 
+                    border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
                   About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                to="/contact"
+                  className={({isActive}) =>
+                    `block py-2 pr-4 pl-3 duration-200 
+                    ${isActive? 'text-orange-700' : 'text-gray-700'} 
+                    border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Contact Us
                 </NavLink>
               </li>
             </ul>
